38b94390bd9000f8e1ef1301c7291a43a9e37c5a
gen <you@example.com>
2020-08-26 00:34:19 +0900

    format

--- lib/network-stack.ts
+++ lib/network-stack.ts
@@ -2,36 +2,36 @@ import { Construct, Stack, StackProps, Tag } from "@aws-cdk/core";
 import { Peer, Port, SecurityGroup, SubnetType, Vpc } from "@aws-cdk/aws-ec2";
 
 export class NetworkStack extends Stack {
-	public readonly vpc: Vpc;
-	public readonly sg: { [key: string]: SecurityGroup; } = {};
+  public readonly vpc: Vpc;
+  public readonly sg: { [key: string]: SecurityGroup; } = {};
 
-	//public readonly dbSg: SecurityGroup;
-	constructor(scope: Construct, id: string, props?: StackProps) {
-		super(scope, id, props);
+  //public readonly dbSg: SecurityGroup;
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
 
-		const env: string = this.node.tryGetContext('env');
-		const app_name: string = this.node.tryGetContext('prj');
-		const suffix: string = `${env}-${app_name}`
-		const params: any = this.node.tryGetContext(env);
+    const env: string = this.node.tryGetContext('env');
+    const app_name: string = this.node.tryGetContext('prj');
+    const suffix: string = `${env}-${app_name}`
+    const params: any = this.node.tryGetContext(env);
 
-		this.vpc = new Vpc(this, `${suffix}-vpc`, {
-			enableDnsHostnames: true,
-			enableDnsSupport: true,
-			maxAzs: 2,
-			subnetConfiguration: [
-				{ name: `${suffix}-public`, subnetType: SubnetType.PUBLIC, cidrMask: 24 },
-				{ name: `${suffix}-private`, subnetType: SubnetType.PRIVATE, cidrMask: 24 }
-			]
-		});
+    this.vpc = new Vpc(this, `${suffix}-vpc`, {
+      enableDnsHostnames: true,
+      enableDnsSupport: true,
+      maxAzs: 2,
+      subnetConfiguration: [
+        { name: `${suffix}-public`, subnetType: SubnetType.PUBLIC, cidrMask: 24 },
+        { name: `${suffix}-private`, subnetType: SubnetType.PRIVATE, cidrMask: 24 }
+      ]
+    });
 
-		this.sg['bastion'] = new SecurityGroup(this, "bastion", {
-			allowAllOutbound: true,
-			vpc: this.vpc,
-			securityGroupName: `${suffix}-bastion`,
-			description: `${suffix}-app`
-		});
+    this.sg['bastion'] = new SecurityGroup(this, "bastion", {
+      allowAllOutbound: true,
+      vpc: this.vpc,
+      securityGroupName: `${suffix}-bastion`,
+      description: `${suffix}-app`
+    });
 
-		this.sg['bastion'].addIngressRule(Peer.anyIpv4(), Port.tcp(22), 'allow ssh connection')
-		this.sg['bastion'].addIngressRule(Peer.anyIpv4(), Port.icmpPing(), 'allow icmp')
-	}
+    this.sg['bastion'].addIngressRule(Peer.anyIpv4(), Port.tcp(22), 'allow ssh connection')
+    this.sg['bastion'].addIngressRule(Peer.anyIpv4(), Port.icmpPing(), 'allow icmp')
+  }
 }
