a54b042d6d42f48718a7fe986aa6341a11579dbb
ito.toshifumi <itotoshifumi@airtrip-corp.com>
2020-09-17 01:09:14 +0900

    refactoring

--- lib/network.ts
+++ lib/network.ts
@@ -1,56 +1,27 @@
-import { Construct } from "@aws-cdk/core";
-import {
-  Vpc,
-  Peer,
-  Port,
-  SubnetType,
-  SecurityGroup,
-} from "@aws-cdk/aws-ec2";
+import cdk   = require("@aws-cdk/core");
+import ec2   = require("@aws-cdk/aws-ec2");
 
-export class Network extends Construct {
-  public readonly vpc: Vpc;
-  public readonly sg: { [key: string]: SecurityGroup; } = {};
+export class Network extends cdk.Construct {
+  public readonly vpc: ec2.Vpc;
 
   //public readonly dbSg: SecurityGroup;
-  constructor(parent: Construct, name: string) {
+  constructor(parent: cdk.Construct, name: string) {
     super(parent, name);
 
     const env: string = this.node.tryGetContext('env');
     const params: any = this.node.tryGetContext(env);
 
     // create vpc
-    this.vpc = new Vpc(this, `${env}-vpc`, {
+    this.vpc = new ec2.Vpc(this, `${env}-vpc`, {
       cidr: params["cibr"],
       enableDnsHostnames: true,
       enableDnsSupport: true,
       maxAzs: params["maxAzs"],
       subnetConfiguration: [
-        { name: `${env}-private`, subnetType: SubnetType.PRIVATE, cidrMask: 24 },
-        { name: `${env}-public`, subnetType: SubnetType.PUBLIC, cidrMask: 24 }
+        { name: `${env}-private`, subnetType: ec2.SubnetType.PRIVATE, cidrMask: 24 },
+        { name: `${env}-public`,  subnetType: ec2.SubnetType.PUBLIC,  cidrMask: 24 }
       ]
     });
-
-    // create security group
-    this.sg['bastion'] = new SecurityGroup(this, "bastion", {
-      vpc: this.vpc,
-      allowAllOutbound: true,
-      securityGroupName: `${env}-bastion`,
-      description: `${env}-bastion`
-    });
-
-    this.sg['private-app'] = new SecurityGroup(this, "private-app-default", {
-      vpc: this.vpc,
-      allowAllOutbound: true,
-      securityGroupName: `${env}-private-app-default`,
-      description: `${env}-private-app-default`
-    });
-
-    // add ingressrule
-    this.sg['bastion'].addIngressRule(Peer.anyIpv4(), Port.tcp(22), 'allow ssh connection')
-
-    // add all icmp ping
-    for (let name in this.sg) {
-      this.sg[name].addIngressRule(Peer.anyIpv4(), Port.icmpPing(), 'allow icmp')
-    };
+    this.vpc.addFlowLog(`${env}-vpc-flow-log`)
   }
 }
